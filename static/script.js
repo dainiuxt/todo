685d1769c9ac5e7258697f131d64c5d3d1e6222c
Plato <gytisnaujasis@icloud.com>
2024-10-03 14:00:38 +0300

    Add taskId var, add id to cbox

--- static/script.js
+++ static/script.js
@@ -3,17 +3,21 @@ const newTaskBtn = taskForm.children['new-task-button'];
 const taskText = taskForm.children['task-text'];
 
 const selectTab = document.getElementById('select-tab');
-const tabs = selectTab.children
+const tabs = selectTab.children;
 
-const tasks = document.getElementById('tasks');
+let cBoxes = document.querySelectorAll('input[type=checkbox]');
 
+let tasks = document.getElementById('tasks');
+
+// Add new task
 newTaskBtn.addEventListener('click', () =>{
     let taskCount = tasks.children.length;
+    let taskId = taskCount;
 
     if (taskText.value) {
         const newTaskListItem = document.createElement('li');
         newTaskListItem.classList.add('list-group-item', 'rounded-3');
-        newTaskListItem.id = `task${taskCount}`;
+        newTaskListItem.id = `task${taskId}`;
 
         // Task text
         // Para elementas reikalingas, nes negalim talpint teksto su input buttonu priesais teksta
@@ -27,6 +31,7 @@ newTaskBtn.addEventListener('click', () =>{
         taskCbox.type = 'checkbox';
         taskCbox.name = 'done-task';
         taskCbox.className = 'me-2';
+        taskCbox.id = `task${taskId}-cb`
         
         newTaskListItem.appendChild(taskCbox);
         newTaskListItem.appendChild(newTaskText);
