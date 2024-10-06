ea50c72a67e3cfc1ab952d4f7d7bebd7781569ca
Plato <gytisnaujasis@icloud.com>
2024-10-06 16:22:04 +0300

    Add completed endpoint

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ todos = [{"task": "take out the trash", "done": False}]
 def index():
     return render_template("layout.html", todos=todos)
 
+# TODO
+@app.route("/active")
+def active():
+    return redirect(url_for("index", todos=todos))
+
+# TODO
+@app.route("/completed")
+def completed():
+    return redirect(url_for("index", todos=todos))
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -20,15 +29,23 @@ def add():
 def edit():
     pass
 
-@app.route("/complete")
-def complete():
-    pass
+@app.route("/complete/<int:task_id>", methods=["POST"])
+def complete(task_id):
+    if request.method == "POST":
+        if not todos[task_id]['done']:
+            todos[task_id]['done'] = True
+        else:
+            todos[task_id]['done'] = False
+
+    return redirect(url_for("index", todos=todos))
 
 @app.route("/delete/<int:task_id>", methods=["POST"])
 def delete(task_id):
     del todos[task_id]
-    
+
     return redirect(url_for("index", todos=todos))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
