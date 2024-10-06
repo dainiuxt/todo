1fd3b195bda069c540ab5f497e37ce90c86c5e5a
Plato <gytisnaujasis@icloud.com>
2024-10-06 15:07:34 +0300

    Add add and display endpoints

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+todos = [{"task": "take out the trash", "done": False}]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("layout.html", todos=todos)
+
+
+# Do form input
+@app.route("/add", methods=["POST"])
+def add():
+    task = request.form['task']
+    todos.append({"task": task, "done": False})
+    return redirect(url_for("index", todos=todos))
+
+@app.route("/edit")
+def edit():
+    pass
+
+@app.route("/complete")
+def complete():
+    pass
+
+@app.route("/delete")
+def delete():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
