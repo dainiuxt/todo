ba4d7149978aa21d10d9560c9fe65a796f48e1a3
Plato <gytisnaujasis@icloud.com>
2024-10-11 20:09:00 +0300

    Update template structure, add ep'oints

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template, url_for,redirect
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_seeder import FlaskSeeder
+# from flask_seeder import FlaskSeeder
 from flask_marshmallow import Marshmallow
 from datetime import datetime
 
@@ -13,36 +13,36 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
+metadata = MetaData(naming_convention=convention)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app, metadata=metadata)
 
-metadata = MetaData(naming_convention=convention)
 ma = Marshmallow(app)
 migrate = Migrate(app, db, render_as_batch=True)
 
-seeder = FlaskSeeder()
-seeder.init_app(app, db)
+# seeder = FlaskSeeder()
+# seeder.init_app(app, db)
 
 
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
     name = db.Column(db.String(50))
 
-    def __init__(self, email, password, name):
-        self.email = email
+    def __init__(self, username, password, name):
+        self.username = username
         self.password = password
         self.name = name
 
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "email", "password", "name")
+        fields = ("id", "username", "password", "name")
 
 
 user_schema = UserSchema()
@@ -78,20 +78,25 @@ tasks_schema = TaskSchema(many=True)
 
 todos = [{"task": "take out the trash", "done": False}]
 
+
+######################################
+############# FRONTEND ###############
+######################################
+
 # All tasks
 @app.route("/")
 def index():
-    return render_template("all.html", todos=todos)
+    return render_template("tasks_all.html", todos=todos)
 
 # Active tasks
 @app.route("/active")
 def active():
-    return render_template("active.html", todos=todos)
+    return render_template("tasks_active.html", todos=todos)
 
 # Completed tasks
 @app.route("/completed")
 def completed():
-    return render_template("completed.html", todos=todos)
+    return render_template("tasks_completed.html", todos=todos)
 
 # Add task
 @app.route("/add", methods=["POST"])
@@ -109,7 +114,7 @@ def edit(task_id):
 
         return redirect(url_for("index", todos=todos))
 
-    return render_template("edit.html", todo=todos[task_id], id=task_id)
+    return render_template("tasks_edit_task.html", todo=todos[task_id], id=task_id)
 
 # Mark as completed
 @app.route("/complete/<int:task_id>", methods=["POST"])
@@ -129,118 +134,138 @@ def delete(task_id):
 
     return redirect(url_for("index", todos=todos))
 
-# create a user
-@app.route("/create_user", methods=["POST"])
-def create_user():
-    email = request.json["email"]
-    password = request.json["password"]
-    name = request.json["name"]
+# Register user
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        pass
+    
+    return render_template("register.html")
 
-    user = User(email, password, name)
-    db.session.add(user)
-    db.session.commit()
+# Login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        pass
+    
+    return render_template("login.html")
 
-    return user_schema.jsonify(user)
+######################################
+############# DATABASE ###############
+######################################
 
+# # create a user
+# @app.route("/create_user", methods=["POST"])
+# def create_user():
+#     username = request.json["username"]
+#     password = request.json["password"]
+#     name = request.json["name"]
 
-@app.route("/users", methods=["GET"])
-def get_users():
-    all_users = User.query.all()
-    result = users_schema.dump(all_users)
-    return jsonify(result)
+#     user = User(username, password, name)
+#     db.session.add(user)
+#     db.session.commit()
 
-
-@app.route("/users/<int:id>", methods=["GET"])
-def get_user(id):
-    user = User.query.get(id)
-    return user_schema.jsonify(user)
-
-
-# update a user
-@app.route("/users/<int:id>", methods=["PUT"])
-def update_user(id):
-    user = User.query.get(id)
-    email = request.json["email"]
-    password = request.json["password"]
-    name = request.json["name"]
-
-    user.email = email
-    user.password = password
-    user.name = name
-
-    db.session.commit()
-
-    return user_schema.jsonify(user)
+#     return user_schema.jsonify(user)
 
 
-@app.route("/users/<int:id>", methods=["DELETE"])
-def delete_user(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-    return user_schema.jsonify(user)
+# @app.route("/users", methods=["GET"])
+# def get_users():
+#     all_users = User.query.all()
+#     result = users_schema.dump(all_users)
+#     return jsonify(result)
 
 
-@app.route("/tasks")
-def get_tasks():
-    all_tasks = Task.query.all()
-    result = tasks_schema.dump(all_tasks)
-    return jsonify(result)
+# @app.route("/users/<int:id>", methods=["GET"])
+# def get_user(id):
+#     user = User.query.get(id)
+#     return user_schema.jsonify(user)
 
 
-# create a task
-@app.route("/create_task", methods=["POST"])
-def create_task():
-    user_id = request.json["user_id"]
-    created = datetime.now()
-    content = request.json["content"]
-    completed = request.json["completed"]
+# # update a user
+# @app.route("/users/<int:id>", methods=["PUT"])
+# def update_user(id):
+#     user = User.query.get(id)
+#     username = request.json["username"]
+#     password = request.json["password"]
+#     name = request.json["name"]
 
-    task = Task(user_id,
-                created=created,
-                content=content,
-                completed=completed)
-    db.session.add(task)
-    db.session.commit()
+#     user.username = username
+#     user.password = password
+#     user.name = name
 
-    return task_schema.jsonify(task)
+#     db.session.commit()
 
-
-# get all tasks for a user
-@app.route("/tasks/<int:user_id>", methods=["GET"])
-def get_user_tasks(user_id):
-    tasks = Task.query.filter_by(user_id=user_id).all()
-    result = tasks_schema.dump(tasks)
-    return jsonify(result)
+#     return user_schema.jsonify(user)
 
 
-# update a task
-@app.route("/tasks/<int:user_id>/<int:id>", methods=["PUT"])
-def update_task(id, user_id):
-    task = Task.query.get(id)
-    id = task.id
-    user_id = user_id
-    content = request.json["content"]
-
-    task.content = content
-
-    db.session.commit()
-    return task_schema.jsonify(task)
+# @app.route("/users/<int:id>", methods=["DELETE"])
+# def delete_user(id):
+#     user = User.query.get(id)
+#     db.session.delete(user)
+#     db.session.commit()
+#     return user_schema.jsonify(user)
 
 
-# complete a task
-@app.route("/tasks/<int:user_id>/<int:id>/complete", methods=["PUT"])
-def complete_task(id, user_id):
-    task = Task.query.get(id)
-    id = task.id
-    user_id = user_id
-    completed = True
-    completed = request.json["completed"]
+# @app.route("/tasks")
+# def get_tasks():
+#     all_tasks = Task.query.all()
+#     result = tasks_schema.dump(all_tasks)
+#     return jsonify(result)
 
-    task.completed = completed
 
-    db.session.commit()
-    return task_schema.jsonify(task)
+# # create a task
+# @app.route("/create_task", methods=["POST"])
+# def create_task():
+#     user_id = request.json["user_id"]
+#     created = datetime.now()
+#     content = request.json["content"]
+#     completed = request.json["completed"]
+
+#     task = Task(user_id,
+#                 created=created,
+#                 content=content,
+#                 completed=completed)
+#     db.session.add(task)
+#     db.session.commit()
+
+#     return task_schema.jsonify(task)
+
+
+# # get all tasks for a user
+# @app.route("/tasks/<int:user_id>", methods=["GET"])
+# def get_user_tasks(user_id):
+#     tasks = Task.query.filter_by(user_id=user_id).all()
+#     result = tasks_schema.dump(tasks)
+#     return jsonify(result)
+
+
+# # update a task
+# @app.route("/tasks/<int:user_id>/<int:id>", methods=["PUT"])
+# def update_task(id, user_id):
+#     task = Task.query.get(id)
+#     id = task.id
+#     user_id = user_id
+#     content = request.json["content"]
+
+#     task.content = content
+
+#     db.session.commit()
+#     return task_schema.jsonify(task)
+
+
+# # complete a task
+# @app.route("/tasks/<int:user_id>/<int:id>/complete", methods=["PUT"])
+# def complete_task(id, user_id):
+#     task = Task.query.get(id)
+#     id = task.id
+#     user_id = user_id
+#     completed = True
+#     completed = request.json["completed"]
+
+#     task.completed = completed
+
+#     db.session.commit()
+#     return task_schema.jsonify(task)
 
 
 if __name__ == "__main__":
